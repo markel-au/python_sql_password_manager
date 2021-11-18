af1bb0d8ad119429fd57f5bb0c867c8075c8de71
markel-au <80484038+markel-au@users.noreply.github.com>
2021-11-18 14:15:20 -0500

    Update db_connect.py

--- db_connect.py
+++ db_connect.py
@@ -2,5 +2,5 @@ import psycopg2
 
 def connection_db(): 
     # Enter password under ******** field. 
-    connection = psycopg2.connect("dbname=/var/run/postgresql user=postgres password=docker") 
+    connection = psycopg2.connect("dbname=2858e66d7777 user=postgres password=docker") 
     return connection
